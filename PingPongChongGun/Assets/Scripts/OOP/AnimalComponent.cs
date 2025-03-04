cd39da14039a81ae4e99185843814a8c06fab549
cruise4u <pedrofrancisco90@gmail.com>
2025-03-04 20:39:14 +0100

    Added Prefab for bear

--- PingPongChongGun/Assets/Scripts/OOP/AnimalComponent.cs
+++ PingPongChongGun/Assets/Scripts/OOP/AnimalComponent.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using TMPro;
 using OOP_Examples;
+using System.Collections;
 
 public class AnimalComponent : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class AnimalComponent : MonoBehaviour
     public void Start()
     {
         animalGoodExample = new OOPClass.OOPEncapsulationExample.AnimalGoodExample();
+        StartCoroutine(WalkRoutine(5, 3));
     }
 
     public void Update()
@@ -41,7 +43,25 @@ public class AnimalComponent : MonoBehaviour
         }
     }
 
+    public IEnumerator WalkRoutine(float seconds, int radius)
+    {
+        while (true)
+        {
+            // Calculate a random point within the circular radius
+            Vector2 randomPoint = Random.insideUnitCircle * radius;
+            Vector3 targetPosition = new Vector3(randomPoint.x, randomPoint.y, transform.position.z);
 
+            // Move to the target position
+            while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                yield return null;
+            }
+
+            // Wait for the specified time
+            yield return new WaitForSeconds(seconds);
+        }
+    }
 
 
 
